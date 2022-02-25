77b5831f0944195f529d09bbe98be2bc3602217c
MFaqihRidho <faqih.fr5@gmail.com>
2022-02-25 09:13:10 +0700

    initialize using react,firebase,and mui

---
+++ src/firebase config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAgzxISdU-9NMwVBQ_uhvx5iBj_HbzW8Hk",
+    authDomain: "to-do-list-project-8c7bb.firebaseapp.com",
+    projectId: "to-do-list-project-8c7bb",
+    storageBucket: "to-do-list-project-8c7bb.appspot.com",
+    messagingSenderId: "821214054608",
+    appId: "1:821214054608:web:8107888cef938bc8c7f6d5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
