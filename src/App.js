5cc68f0808daeddf32704b3df2538fab59972e31
MFaqihRidho <faqih.fr5@gmail.com>
2022-02-25 15:18:25 +0700

    adding dark mode switch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./pages/home";
+import Navbar from "./components/navbar";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [theme, setTheme] = useState(false);
+
+    return (
+        <div className="App">
+            <Navbar></Navbar>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/firebase config.js
+++ src/firebase config.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAgzxISdU-9NMwVBQ_uhvx5iBj_HbzW8Hk",
     authDomain: "to-do-list-project-8c7bb.firebaseapp.com",
@@ -13,5 +10,5 @@ const firebaseConfig = {
     appId: "1:821214054608:web:8107888cef938bc8c7f6d5",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const dataBase = getFirestore(app);
