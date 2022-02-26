1646a621b35edc82313c258417d82bb922517027
MFaqihRidho <faqih.fr5@gmail.com>
2022-02-26 13:27:47 +0700

    dark mode

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import "./App.css";
 import Home from "./pages/home";
 import Navbar from "./components/navbar";
 import { Routes, Route } from "react-router-dom";
-import { useState } from "react";
-import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { Paper } from "@mui/material";
 import { useSelector } from "react-redux";
-import { height } from "@mui/system";
 
 function App() {
     const mode = useSelector((state) => state.theme);
 
     const theme = createTheme({
         palette: {
-            mode: mode,
+            mode: localStorage.getItem("theme"),
         },
     });
 
